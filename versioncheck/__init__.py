23f03b0c591eda95cd6e1706004e2b6d47f40fb7
noxan <noxan@byteweaver.net>
2013-05-25 02:22:36 +0200

    remove buggy calls in module init script

--- versioncheck/__init__.py
+++ versioncheck/__init__.py
@@ -1,8 +1,1 @@
 __version__ = '0.0.1'
-
-import utils
-
-
-if __name__ == "__main__":
-    print "Current version: %s" % (utils.get_version())
-    print "Latest version: %s" % (utils.get_lastest_version())
