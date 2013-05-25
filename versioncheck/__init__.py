8b4f63e2b1f775c302d07d4a192a566605d22563
noxan <noxan@byteweaver.net>
2013-05-25 02:12:44 +0200

    display current and latest version in main

--- versioncheck/__init__.py
+++ versioncheck/__init__.py
@@ -1,1 +1,8 @@
 __version__ = '0.0.1'
+
+import utils
+
+
+if __name__ == "__main__":
+    print "Current version: %s" % (utils.get_version())
+    print "Latest version: %s" % (utils.get_lastest_version())
