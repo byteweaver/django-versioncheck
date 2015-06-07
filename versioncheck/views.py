9a965cab4bbd77cf2c78165b4f3aa5c55aaa610b
Richard <richard@devisio.net>
2015-06-07 11:29:57 +0200

    implement small view to return latest and current version as json, #2

--- versioncheck/views.py
+++ versioncheck/views.py
@@ -1,1 +1,12 @@
-# Create your views here.
+from django.http import JsonResponse
+from django.views.generic import View
+
+from versioncheck import utils
+
+
+class VersionView(View):
+    def get(self, request):
+        return JsonResponse({
+            'current': utils.get_version(),
+            'latest': utils.get_latest_version(),
+        })
